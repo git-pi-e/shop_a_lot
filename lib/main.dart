8be2cd3e0ca20b569fe764a90dfb116932e5a7b6
Piyush Mohite <piyushm17mar@gmail.com>
2021-08-07 16:07:55 +0530

    Made kanding page a StreamBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shop_a_lot/screens/landing_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -11,70 +11,75 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      builder: (context, snapshot) {
+    return StreamBuilder(
+      builder: (context, streamSnapshot) {
+        stream:
+        FirebaseAuth.instance.authStateChanges();
+        Object? _user =
+            streamSnapshot.data; // If the user is null, we're not logged in
+        if (_user == null) {
+          // user not logged in, head to login
+          return LoginPage();
+        } else {
+          // The user is logged in, head to homepage
+          return HomePage();
+        }
+
         // If Firebase App init, snapshot has error
-        if (snapshot.hasError) {
-          return Scaffold(
-            body: Center(
-              child: Text('Error: ${snapshot.error}'),
-            ),
-          );
-        }
+//         if (snapshot.hasError) {
+//           return Scaffold(
+//             body: Center(
+//               child: Text('Error: ${snapshot.error}'),
+//             ),
+//           );
+//         }
 
-        // Connection Initialized - Firebase App is running
-        if (snapshot.connectionState == ConnectionState.done) {
-          // StreamBuilder can check the login state live
-          return StreamBuilder(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, streamSnapshot) {
-              // If Stream Snapshot has error
-              if (streamSnapshot.hasError) {
-                return Scaffold(
-                  body: Center(
-                    child: Text("Error: ${streamSnapshot.error}"),
-                  ),
-                );
-              }
+//         // Connection Initialized - Firebase App is running
+//         if (snapshot.connectionState == ConnectionState.done) {
+//           // StreamBuilder can check the login state live
+//           return StreamBuilder(
+//             stream: FirebaseAuth.instance.authStateChanges(),
+//             builder: (context, streamSnapshot) {
+//               // If Stream Snapshot has error
+//               if (streamSnapshot.hasError) {
+//                 return Scaffold(
+//                   body: Center(
+//                     child: Text("Error: ${streamSnapshot.error}"),
+//                   ),
+//                 );
+//               }
 
-              // Connection state active - Do the user login check inside the
-              // if statement
-              if (streamSnapshot.connectionState == ConnectionState.active) {
-                // Get the user
-                Object? _user = streamSnapshot.data;
+//               // Connection state active - Do the user login check inside the
+//               // if statement
+//               if (streamSnapshot.connectionState == ConnectionState.active) {
+//                 // Get the user
+//
 
-                // If the user is null, we're not logged in
-                if (_user == null) {
-                  // user not logged in, head to login
-                  return LoginPage();
-                } else {
-                  // The user is logged in, head to homepage
-                  return HomePage();
-                }
-              }
+// ///////////////////////////////////
+//               }
 
-              // Checking the auth state - Loading
-              return Scaffold(
-                body: Center(
-                  child: Text(
-                    "Checking Authentication...",
-                    style: Constants.regularHeading,
-                  ),
-                ),
-              );
-            },
-          );
-        }
+//               // Checking the auth state - Loading
+//               return Scaffold(
+//                 body: Center(
+//                   child: Text(
+//                     "Checking Authentication...",
+//                     style: Constants.regularHeading,
+//                   ),
+//                 ),
+//               );
+//             },
+//           );
+//         }
 
         // Connecting to Firebase - Loading
-        return Scaffold(
-          body: Center(
-            child: Text(
-              "Initializing App...",
-              style: Constants.regularHeading,
-            ),
-          ),
-        );
+        // return Scaffold(
+        //   body: Center(
+        //     child: Text(
+        //       "Initializing App...",
+        //       style: Constants.regularHeading,
+        //     ),
+        //   ),
+        // );
       },
     );
   }
